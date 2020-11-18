d9aaa29465c1ac8a7d4436b1a22e1f368fd0f3d8
Molly <dolanmollye@gmail.com>
2020-11-18 10:28:14 -0800

    pair programming branch1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account, :happiness_points, :hygiene_point
+    attr_reader :name
+
+    def initialize(name, bank_account = 25, happiness_points = "8", hygiene_points = "8")
+        @name = name
+        @bank_account = bank_account
+        @happiness_points = happiness_points
+        @hygiene_point = hygiene_points
+    end
+    
+    # def name
+    #     @name
+    # end
+
+end
