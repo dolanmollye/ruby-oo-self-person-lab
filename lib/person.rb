44fdc15ab65fceaac9d4c4889406fc49fee73405
Molly <dolanmollye@gmail.com>
2020-11-18 13:31:49 -0800

    commit tree c4661484f1a44085286c4c8839182a62789c75ec

--- lib/person.rb
+++ lib/person.rb
@@ -65,6 +65,11 @@ class Person
         "♪ another one bites the dust ♫"
     end
 
-    def call_friend()
+    def call_friend(friend)
+      self.happiness += 3
+      friend.happiness += 3
+      "Hi #{friend.name}! It's #{self.name}. How are you?"
+        binding.pry
+    end
 
 end
