dad96bb62f5e47e37d4522da9b6916e56d0832fe
Hunter Posey <ihposey@crimson.ua.edu>
2020-11-18 11:26:46 -0800

    finished happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,23 @@
 # your code goes here
+require "pry"
+
 class Person
-    attr_accessor :bank_account, :happiness_points, :hygiene_point
+    attr_accessor :bank_account, :happiness, :hygiene
     attr_reader :name
 
-    def initialize(name, bank_account = 25, happiness_points = "8", hygiene_points = "8")
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
-        @happiness_points = happiness_points
-        @hygiene_point = hygiene_points
+        @happiness = happiness
+        @hygiene = hygiene
     end
-    
-    # def name
-    #     @name
-    # end
+
+    def happiness=(num)
+        if num < 11
+            @happiness = num
+        elsif num > 10
+            @happiness = 10
+        end
+    end
 
 end
